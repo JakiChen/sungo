db81101ddfc21100d9c709ec09b37459b0628a62
Jaki Chen <jaki@tradesparq.com>
2015-03-31 17:15:18 +0800

    Update

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// Tamino Martinius - All rights reserved
+// Copyright © 2013 Tamino Martinius (http://zaku.eu)
+
+// Get ParticleSlider: http://particleslider.com/
+
+//var ps = new ParticleSlider({
+//    sliderId: 'particle-slider-1' //id of HTML-Element
+//  , slideDelay: 4.0 //delay between slides
+//});
+var ps = new ParticleSlider({
+    sliderId: 'particle-slider-2' //id of HTML-Element
+  , slideDelay: 15.0 //delay between slides
+});
+//var ps = new ParticleSlider({
+//    sliderId: 'particle-slider-3' //id of HTML-Element
+//  , slideDelay: 4.0 //delay between slides
+//});
